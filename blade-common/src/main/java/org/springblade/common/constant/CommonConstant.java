0b6f0a33bc871f76db805f04f5b410a630afd183
smallchill <smallchill@163.com>
2019-03-18 14:13:43 +0800

    :zap: 修复sentinel地址

--- blade-common/src/main/java/org/springblade/common/constant/CommonConstant.java
+++ blade-common/src/main/java/org/springblade/common/constant/CommonConstant.java
@@ -25,7 +25,7 @@ public interface CommonConstant {
 	/**
 	 * sentinel prod 地址
 	 */
-	String SENTINEL_PROD_ADDR = "172.30.0.48:8858";
+	String SENTINEL_PROD_ADDR = "172.30.0.58:8858";
 
 	/**
 	 * sword 系统名
