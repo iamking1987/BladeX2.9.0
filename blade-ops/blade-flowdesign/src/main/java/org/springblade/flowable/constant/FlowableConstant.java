43a454cc304f073b29627d4fb9131f5b81879a2a
smallchill <smallchill@163.com>
2019-04-09 17:28:16 +0800

    :zap: 增加流程设计器

---
+++ blade-ops/blade-flowdesign/src/main/java/org/springblade/flowable/constant/FlowableConstant.java
@@ -0,0 +1,17 @@
+package org.springblade.flowable.constant;
+
+/**
+ * Flowable常量
+ *
+ * @author Chill
+ */
+public interface FlowableConstant {
+	String BASE_PACHAGE = "org.springblade";
+	String FLOWABLE_BASE_PACKAGE = "org.flowable.ui";
+
+	String css = ".css";
+	String js = ".js";
+	String jpg = ".jpg";
+	String png = ".png";
+	String app = "/app";
+}
